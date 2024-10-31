c0966a60610a833dd961264bca9d1466cc21f449
diego-gv <diegosalvador.gv@gmail.com>
2024-10-31 17:01:47 +0100

    devtools installation: postman and dbeaver

---
+++ scripts/installs/devtools.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")" \
+    && . "../utils.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    print_in_purple "\n   DevTools\n\n"
+
+    "./$(get_os)/devtools.sh"
+
+}
+
+main
