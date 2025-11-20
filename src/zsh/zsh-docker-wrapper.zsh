a3d53fc511b9607d7b561764081b03094d4c2802
diego-gv <diegosalvador.gv@gmail.com>
2025-11-20 09:07:36 +0100

    feat: update docker wrapper

--- src/zsh/zsh-docker-wrapper.zsh
+++ src/zsh/zsh-docker-wrapper.zsh
@@ -1,40 +1,60 @@
 #!/usr/bin/env zsh
 # vim: set filetype=zsh
 
-# Colores
 GREEN="\033[0;32m"
 YELLOW="\033[1;33m"
 RED="\033[0;31m"
 BLUE="\033[0;34m"
 NC="\033[0m"
+FG_ALT="\033[38;5;253m"
+
+_docker_ps() {
+    command docker ps --format "{{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}" | \
+    awk -F'\t' \
+        -v green="$GREEN" -v yellow="$YELLOW" -v red="$RED" \
+        -v blue="$BLUE" -v nc="$NC" -v fg_alt="$FG_ALT" '
+        BEGIN {
+            printf "%sCONTAINER ID\tIMAGE\tNAMES\tSTATUS\tPORTS%s\n", blue, nc
+        }
+        {
+            row++
+            id=$1; image=$2; name=$3; status=$4; ports=$5
+
+            emoji=""; color=nc
+            if (status ~ /^Up/)          { emoji="✅"; color=green }
+            else if (status ~ /^Exited/) { emoji="❌"; color=red }
+            else if (status ~ /^Paused/) { emoji="⏸️"; color=yellow }
+
+            status=color emoji " " status nc
+
+            fg = (row % 2 == 1) ? fg_alt : nc
+
+            if (ports == "") ports="-"
+            n = split(ports, port_arr, ",")
+
+            for (i=1; i<=n; i++) {
+                gsub(/^ +| +$/, "", port_arr[i])
+                if (i == 1) {
+                    printf "%s%s\t%s\t%s\t%s\t%s%s%s\n", fg, id, image, name, status, fg, port_arr[i], nc
+                } else {
+                    printf "%s\t%s\t%s\t%s\t%s%s\n", fg, "", "", "", port_arr[i], nc
+                }
+            }
+        }
+    ' | column -t -s $'\t'
+}
 
 docker() {
+    if [[ "$1" == "ps" ]]; then
+        _docker_ps "$@"
+        return 0
 
-  if [[ "$1" == "ps" ]]; then
-    command docker ps --format "{{.ID}}\t{{.Image}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}" | \
-    awk -F'\t' -v green="$GREEN" -v yellow="$YELLOW" -v red="$RED" -v blue="$BLUE" -v nc="$NC" '
-      NR==1 {
-        printf "%s%s%s\t%s%s%s\t%s%s%s\t%s%s%s\t%s%s%s\n", blue, "CONTAINER ID", nc, blue, "IMAGE", nc, blue, "NAMES", nc, blue, "STATUS", nc, blue, "PORTS", nc
-        next
-      }
-      {
-        id=$1; image=$2; name=$3; status=$4; ports=$5
-        emoji=""; color=nc
-        if (status ~ /^Up/)          { emoji="✅"; color=green }
-        else if (status ~ /^Exited/) { emoji="❌"; color=red }
-        else if (status ~ /^Paused/) { emoji="⏸️"; color=yellow }
-        status_colored=color emoji " " status nc
-        n=split(ports, port_arr, ",")
-        for (i=1; i<=n; i++) {
-          gsub(/^ +| +$/, "", port_arr[i])
-          if (i==1) { printf "%s\t%s\t%s\t%s\t%s\n", id, image, name, status_colored, port_arr[i] }
-          else      { printf "%s\t%s\t%s\t%s\t%s\n", "", "", "", "", port_arr[i] }
-        }
-      }
-    ' | column -t -s $'\t'
-    return 0
-  fi
+    # Otros comandos de docker se pueden añadir aquí
+    # elif [[ "$1" == "stats" ]]; then
+    #     _docker_stats "$@"
+    #     return 0
 
-  # Todos los demás comandos Docker
-  command docker "$@"
+    else
+        command docker "$@"
+    fi
 }
