1b58239888709d974dad68ea60b6573c600ee214
diego-gv <diegosalvador.gv@gmail.com>
2026-01-13 13:20:32 +0100

    refactor: simplify dotfiles alias

--- src/zsh/zsh-utils.zsh
+++ src/zsh/zsh-utils.zsh
@@ -2,11 +2,9 @@
 # vim: set filetype=zsh
 
 function dotfiles() {
-    if command -v code-insiders &>/dev/null; then
-        code-insiders ~/.dotfiles
-    elif command -v code &>/dev/null; then
+    if command -v code &>/dev/null; then
         code ~/.dotfiles
     else
-        echo -e "\033[0;31mYou need to have code-insiders or code installed to use this alias.\033[0m"
+        echo -e "\033[0;31mYou need to have code (vscode) installed to use this alias.\033[0m"
     fi
 }
